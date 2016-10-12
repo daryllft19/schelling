2b12b58d509a67bf323693b1eda9975822ed04b1
Daryll Tumambing <daryllft19@gmail.com>
2016-10-13 00:42:15 +0800

    added grid class

--- exam.py
+++ exam.py
@@ -1,6 +1,21 @@
 #! /usr/bin/python
 import numpy
 
+'''
+Grid object. To store the number of x's and o's
+'''
+class Grid(object):
+	
+	def __init__(self, array):
+		self.array = array
+		self.x = (array == 'X').sum() 
+		self.o = (array == 'O').sum() 
+
+	
+
+'''
+Accepts a file parameter then converts the contents into a 2d array
+'''
 def loadgrid(file):
 	list = []
 	length = 0
@@ -12,5 +27,4 @@ def loadgrid(file):
 
 	length, width = numpy.shape(list)
 
-	return numpy.array(list)
-
+	return Grid(numpy.array(list))
